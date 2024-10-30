945695380d4f63be10cc38ec556a0ea43b4a4622
Wale <56082759+WaleAwo@users.noreply.github.com>
2024-10-30 11:18:51 +0000

    first commit

---
+++ sum.test.js
@@ -0,0 +1,9 @@
+test('adds 2 + 2 to equal 4', () => {
+    let result = 2 + 2
+    expect(result).toBe(4);
+});
+
+test('adds 3 + 5 to equal 7', () => {
+    let result = 3 + 5
+    expect(result).toBe(7);
+});
