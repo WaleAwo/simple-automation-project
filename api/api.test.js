c28ca0f457bb19b1dcbb9080e00e1240bd38b31d
Wale <56082759+WaleAwo@users.noreply.github.com>
2024-11-01 14:25:36 +0000

    Added parameterized test

--- api/api.test.js
+++ api/api.test.js
@@ -10,6 +10,20 @@ test("test for an API - retrieving data", async () => {
     expect(response.data['company']['bs']).toEqual("harness real-time e-markets");
 });
 
+test.each`
+        id   | name                  | bs                                 
+        ${1} | ${'Leanne Graham'}    | ${'harness real-time e-markets'}  
+        ${2} | ${'Ervin Howell'}     | ${'synergize scalable supply-chains'}
+        ${3} | ${'Clementine Bauch'}  | ${'e-enable strategic applications'}
+    `('user $id should return $name and company $bs', async ({id, name, bs}) => {
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`);
+
+    expect(response.status).toBe(200);
+    expect(response.headers['content-type']).toBe('application/json; charset=utf-8');
+    expect(response.data['name']).toEqual(name);
+    expect(response.data['company']['bs']).toEqual(bs);
+});
+
 test("test for an API - submitting data", async () => {
     const response = await axios.post('https://jsonplaceholder.typicode.com/posts', {
         userId: 1,
